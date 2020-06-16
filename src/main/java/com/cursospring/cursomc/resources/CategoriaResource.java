a6eddfbbe54db96a25391b183c40d5ed6c330630
Fillipe Soares <felipesoares_1993@hotmail.com>
2020-06-16 15:40:37 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/cursospring/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursospring/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.cursospring.cursomc.resources;
 
 import com.cursospring.cursomc.domain.Categoria;
 import com.cursospring.cursomc.services.CategoriaService;
+import com.cursospring.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +18,10 @@ public class CategoriaResource {
     private CategoriaService categoriaService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<?> find(@PathVariable Integer id) {
+    public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
 
-        Categoria obj = categoriaService.buscar(id);
+
+        Categoria obj = categoriaService.find(id);
 
         return ResponseEntity.ok().body(obj);
     }
