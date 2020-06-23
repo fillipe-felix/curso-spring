a6e332a953b8e4ac6ce9fb16cbfbb4df31eab915
Fillipe Soares <felipesoares_1993@hotmail.com>
2020-06-23 19:02:36 -0300

    Listando todas as "Categorias" com DTO, e adicionando alguns comentarios

--- src/main/java/com/cursospring/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursospring/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.cursospring.cursomc.resources;
 
 import com.cursospring.cursomc.domain.Categoria;
+import com.cursospring.cursomc.dto.CategoriaDTO;
 import com.cursospring.cursomc.services.CategoriaService;
 import com.cursospring.cursomc.services.exceptions.DataIntegrityException;
 import com.cursospring.cursomc.services.exceptions.ObjectNotFoundException;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -27,6 +30,17 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> categoriaList = categoriaService.findAll();
+        //percorre a categoriaList e transforma em um elemento categoriaDTOList para que nao retorne os produtos na
+        // lista de categorias
+        List<CategoriaDTO> categoriaDTOList = categoriaList.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(categoriaDTOList);
+    }
+
+    //Foi utilizado URI para que no retorno da requisição post seja retornado o metodo http 201 e a uri do obj criado
+    // no header da requisição
     @PostMapping
     public ResponseEntity<Categoria> insert(@RequestBody Categoria obj){
         obj = categoriaService.insert(obj);
@@ -34,6 +48,7 @@ public class CategoriaResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    //noContent para que retorne  http 204
     @PutMapping(value = "/{id}")
     public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria obj) throws ObjectNotFoundException {
         obj = categoriaService.update(id, obj);
